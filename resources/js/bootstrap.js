8c1573a2e9649147df75b0fbfd744022f34b2160
Carlos Ferreira <conta@especializati.com.br>
2021-03-25 17:39:01 -0300

    Fix IP Laravel ECHO

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -46,7 +46,7 @@ window.io = require('socket.io-client');
 
 window.Echo = new Echo({
     broadcaster: 'socket.io',
-    host: process.env.MIX_HOSTNAME + ':6001'
+    host: 'http://143.110.239.44:6001'
 });
 
 require('./Echo');
